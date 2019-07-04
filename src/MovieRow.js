b67ce8182cc327411e613e0aabe73730fbf62231
guptasarbesh <sarveshg821@gmail.com>
2019-07-04 10:53:01 +0530

    Commit

--- src/MovieRow.js
+++ src/MovieRow.js
@@ -5,19 +5,19 @@ class MovieRow extends React.Component{
     {
         return <div className="container-fluid"  key={this.props.movie.id}>
       
-        <div className="row col-sm-12">
+     <div className="row col-sm-12">
         <div className="col-sm-4">
             
             <img className="card-img-top" height="225px"width="50" alt="poster" src={this.props.movie.poster_src}/>
-      </div>    
+         </div>    
 
-      <div className="col-sm-8">
+        <div className="col-sm-8">
       
               <h3> {this.props.movie.title}</h3>
               <p>{this.props.movie.overview}</p>
            
-      </div>
-        </div>
+         </div>
+     </div>
         
 
     
